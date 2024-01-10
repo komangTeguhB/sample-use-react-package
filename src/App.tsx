1a8d16873b62cedd606dad0ea5a99b774c02dc39
Komang Teguh Budiyana <komz_boz@yahoo.co.id>
2024-01-10 16:21:08 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Dropdown } from "explore-react-ui-library";
+import "explore-react-ui-library/lib/components/style.css";
 
 function App() {
+
+  const showSelection = (selectedOption: string) => {
+    console.log(selectedOption)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="px-4 py-2 space-y-2">
+    <div>
+      <Button
+        text="Testing Primary Button"
+        type="primary"
+        customClasses="px-4 py-2"
+        onClick={() => { console.log("Primary"); }} />
     </div>
+    <div>
+      <Button
+        text="Testing Secondary Button"
+        type="secondary"
+        customClasses="px-4 py-2"
+        onClick={() => { console.log("Secondary"); }} />
+    </div>
+    <Dropdown
+      options={["Test 1", "Test 2", "Test 3"]}
+      containerClasses=""
+      buttonClasses="border border-gray-500 px-4 py-2"
+      menuClasses="border border-gray-500 px-4"
+      onSelect={showSelection}
+      defaultCaption="Choose option" />
+   </div>
   );
 }
 
